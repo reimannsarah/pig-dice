705638e415289d58fa3688ccc11b4af48107250d
Sarah Reimann <reimannsarah45@gmail.com>
2023-04-05 11:55:10 -0700

    WIP: run turn flips turns but pushes global array
    co-authored-by: Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
    dquote> co-authored-by: Emma Gerigscott <elgerig@gmail.com>

--- js/script.js
+++ js/script.js
@@ -12,31 +12,50 @@ TotalScore.prototype.addTurnScore = function (score) {
     this.scores[this.assignRound()] = score;
 }
 
-function TurnScore(userScore, computerScore) {
-    this.userScore = userScore;
-    this.computerScore = computerScore;
+function TurnScore() {
+    this.userScore = [];
+    this.computerScore = [];
 }
+//  Global variables
+let scoreTracker = [];
+let user = 1;
 
 function diceRoll() {
     const dice = Math.floor(Math.random() * 6) + 1;
     return dice;
 }
 
-let scoreTracker = [];
-
-function scoreArray() {
+function scoreArrayer() {
     let roll = diceRoll();
-    if(roll !== 1){
+    if (roll !== 1) {
         scoreTracker.push(roll);
     } else { scoreTracker = [] }
-    return scoreTracker;
 }
 
-// function diceCheck(dice) {
-//     if (dice !== 1) {
-//         return dice;
-//     } else { return false }
-// }
+function turnFlipper() {
+    if (user === 1) {
+        user = 2;
+    } else { user = 1; }
+}
+
+let newTurn = new TurnScore()
+
+function runTurn() {
+    if (user === 1) {
+        scoreArrayer();
+        newTurn.userScore.push(scoreTracker);
+            if (scoreTracker.length === 0) {
+                turnFlipper()
+            }
+        } else {
+            scoreArrayer();
+            newTurn.computerScore.push(scoreTracker);
+            if (scoreTracker.length === 0) {
+                turnFlipper();
+            } 
+    }
+    return newTurn
+}
 
 // function addScore() {
 //     let scoreArray = [];
